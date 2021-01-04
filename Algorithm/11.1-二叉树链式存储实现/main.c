7a8ea5d3ba1a711c7586652439cd0e715985e521
石玉龙 <iosshiyulong@yeah.net>
2021-01-04 10:39:29 +0800

    11.1-二叉树链式存储实现 void PostOrderTraverse(BiTree T) {}

--- Algorithm/11.1-二叉树链式存储实现/main.c
+++ Algorithm/11.1-二叉树链式存储实现/main.c
@@ -37,10 +37,127 @@ Status StrAssign(String T,char *chars) {
 typedef char CElemType;
 CElemType NIL=' ';
 typedef struct BiTNode{
-    
-};
+    CElemType data;
+    struct BiTNode *lchild,*rchild;
+}BiTNode,*BiTree;
+
+Status visit(CElemType e) {
+    printf("%c ",e);
+    return OK;
+}
+Status InitBiTree(BiTree *T) {
+    *T=NULL;
+    return OK;
+}
+void DestroyBiTree(BiTree *T) {
+    if (*T) {
+        if ((*T)->lchild) {
+            DestroyBiTree(&(*T)->lchild);
+        }
+        if ((*T)->rchild) {
+            DestroyBiTree(&(*T)->rchild);
+        }
+        free(*T);
+        *T=NULL;
+    }
+}
+#define ClearBiTree DestroyBiTree
+void CreateBiTree(BiTree *T) {
+    CElemType ch;
+    ch=str[indexs++];
+    if (ch=='#') {
+        *T=NULL;
+    }else{
+        *T=(BiTree)malloc(sizeof(BiTNode));
+        if (!*T) {
+            exit(OVERFLOW);
+        }
+        (*T)->data=ch;
+        CreateBiTree(&(*T)->lchild);
+        CreateBiTree(&(*T)->rchild);
+    }
+}
+Status BiTreeEmpty(BiTree T) {
+    if (T) {
+        return FALSE;
+    }
+    return TRUE;
+}
+int BiTreeDepth(BiTree T) {
+    int i,j;
+    if (!T) {
+        return 0;
+    }
+    if (T->lchild) {
+        i=BiTreeDepth(T->lchild);
+    }else{
+        i=0;
+    }
+    if (T->rchild) {
+        j=BiTreeDepth(T->rchild);
+    }else{
+        j=0;
+    }
+    return i>j?i+1:j+1;
+}
+CElemType Root(BiTree T) {
+    if (BiTreeEmpty(T)) {
+        return NIL;
+    }
+    return T->data;
+}
+CElemType Value(BiTree p) {
+    return p->data;
+}
+void Assign(BiTree p,CElemType value) {
+    p->data=value;
+}
+void PreOrderTraverse(BiTree T) {
+    if (T==NULL) {
+        return;
+    }
+    printf("%c",T->data);
+    PreOrderTraverse(T->lchild);
+    PreOrderTraverse(T->rchild);
+}
+void InOrderTraverse(BiTree T) {
+    if (T==NULL) {
+        return;
+    }
+    InOrderTraverse(T->lchild);
+    printf("%c",T->data);
+    InOrderTraverse(T->rchild);
+}
+void PostOrderTraverse(BiTree T) {
+    if (T==NULL) {
+        return;
+    }
+    PostOrderTraverse(T->lchild);
+    PostOrderTraverse(T->rchild);
+    printf("%c",T->data);
+}
 
 int main(int argc, const char * argv[]) {
+    int i;
+    BiTree T;
+    CElemType e1;
+    InitBiTree(&T);
+    StrAssign(str, "ABDH#K###E##CFI###G#J##");
+    CreateBiTree(&T);
+    printf("二叉树是否为空%d(1:是 0:否),树的深度=%d\n",BiTreeEmpty(T),BiTreeDepth(T));
     
+    e1=Root(T);
+    printf("二叉树的根为: %c\n",e1);
+    
+    printf("\n前序遍历二叉树:");
+    PreOrderTraverse(T);
+    
+    printf("\n中序遍历二叉树:");
+    InOrderTraverse(T);
+    
+    printf("\n后序遍历二叉树:");
+    PostOrderTraverse(T);
+    
+    printf("\n");
     return 0;
 }
